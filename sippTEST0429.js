ef78ea8d5af3a4771fa493d87e2f49cf22d9bce3
nctutwtlab <40228969+nctutwtlab@users.noreply.github.com>
2019-04-29 09:48:18 +0800

    Scratch_course_test
    for teacher test

---
+++ sippTEST0429.js
@@ -0,0 +1,107 @@
+
+(function(ext) {
+    var current_step = 0;
+    var init_step = 0;
+    var database;
+    var address;
+    var way = 'USB';
+    var status = 1;
+    var localStored = 0;
+    var localCurrent = 0;
+
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {
+        database.goOffline();
+        steps = 0;
+        status = 1;
+    };
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/services/thing/status/system',
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            success: (data) => {
+                var obj = JSON.parse(data);
+                if (obj.status === "READY") {
+                    status = 2;
+                } else {
+                    status = 1;
+                }
+                msg = obj.status;
+            },
+            err: (textStatus, errorThrown) => {
+                console.log(textStatus);
+                console.log(errorThrown);
+                status = 0;
+                msg = "ERROR";
+            },
+        });
+        return {status: status, msg: msg};
+    };
+
+    ext.stored_step = function (callback) {
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/services/pedometer/data/step',
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            success: (data) => {
+                console.log(data);
+                var obj = JSON.parse(data);
+                localStored = obj.value;
+                callback(localStored);
+            },
+            err: (textStatus, errorThrown) => {
+                console.log(textStatus);
+                console.log(errorThrown);
+                callback(0);
+            },
+        });
+    };
+
+    ext.current_step = function (callback) {
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/services/pedometer/data/step',
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            success: (data) => {
+                console.log(data);
+                
+                var obj = JSON.parse(data);
+                if (localStored == 0) {
+                    localStored = obj.value;
+                }
+                var localCurrent = obj.value - localStored;
+                localStored = obj.value;
+                callback(localCurrent);
+            },
+            err: (textStatus, errorThrown) => {
+                console.log(textStatus);
+                console.log(errorThrown);
+                callback(0);
+            },
+        });
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', '觸發', 'stored_step'], 
+            ['R', 'AccX', 'accx'], 
+            ['R', 'AccY', 'accy'], 
+            ['R', 'AccZ', 'accz'],
+            ['R', 'GyroX', 'gyrox'], 
+            ['R', 'GyroY', 'gyroy'], 
+            ['R', 'GyroZ', 'gyroz']
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('SippSensor', descriptor, ext);
+})({});
